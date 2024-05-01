2b7b5be1c3b71edb974081b4f43d1429f88e6f63
kazibilsi <kazibilasi@gmail.com>
2024-05-01 20:36:43 +0600

    completed navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 export const Navbar = () => {
+    const [menu, setMenu] = useState('shop')
     return (
         <div className='navbar'>
             <div className='nav-logo'>
@@ -13,14 +14,17 @@ export const Navbar = () => {
 
             </div>
             <ul className='nav-menu'>
-                <li>shop</li>
-                <li>shop</li>
-                <li>shop</li>
-                <li>shop</li>
+                <li onClick={() => { setMenu('shop') }}>Shop{menu === "shop" ? <hr></hr>:<></>}</li>
+                <li onClick={() => { setMenu('mens') }}>Men {menu === "mens" ? <hr></hr>:<></>}</li>
+                <li onClick={() => { setMenu('womens') }}>Women {menu === "womens" ? <hr></hr>:<></>}</li>
+                <li onClick={() => { setMenu('kids') }}>Kids {menu === "kids" ? <hr></hr>:<></>}</li>
             </ul>
             <div className='nav-login-cart'>
                 <button>Login</button>
                 <img src={cart_icon}></img>
+                <div className='nav-cart-count'>
+                    0
+                </div>
             </div>
         </div>
     )
